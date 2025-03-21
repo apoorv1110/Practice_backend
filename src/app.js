caec1445c1ad26235f31ccf39b22e56f7ada9aa3
apoorv1110 <apoorvmittal9@gmail.com>
2025-03-21 15:16:52 +0530

    utilies

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from "cors"
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+// configurations 
+
+app.use(express.json({limit: "16kb"})) // data from form (input specifically from user)
+app.use(express.urlencoded({extended: true , limit:"16kb"})) // data from url
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("ERRR:",error );
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed" , err);
+    
+})
 
 
 
